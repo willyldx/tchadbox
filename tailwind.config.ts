624d8be5c775c9e0255bc6aac35b7e07b4d9a606
WLY <spencer62@protonmail.com>
2026-03-18 14:26:24 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Amazon Ember"', 'Arial', 'sans-serif'],
+        display: ['"Amazon Ember"', 'Arial', 'sans-serif'],
+        body: ['"Amazon Ember"', 'Arial', 'sans-serif'],
+      }
+    }
+  }
+} satisfies Config
